4ea30c488c202fa7e15446473c7c99f689ec330c
slartus <slartus@gmail.com>
2015-03-13 12:35:19 +0500

    поиск

--- app/src/main/java/org/softeg/slartus/forpdaplus/search/SearchPostsParser.java
+++ app/src/main/java/org/softeg/slartus/forpdaplus/search/SearchPostsParser.java
@@ -1,8 +1,13 @@
 package org.softeg.slartus.forpdaplus.search;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.softeg.slartus.forpdaplus.App;
 import org.softeg.slartus.forpdaplus.classes.Exceptions.MessageInfoException;
 import org.softeg.slartus.forpdaplus.classes.HtmlBuilder;
@@ -23,37 +28,35 @@ import java.util.regex.Pattern;
  * Time: 12:54
  * To change this template use File | Settings | File Templates.
  */
-public class SearchPostsParser extends HtmlBuilder  {
+public class SearchPostsParser extends HtmlBuilder {
     private boolean m_SpoilerByButton = false;
-    private Hashtable<String,String> m_EmoticsDict;
+    private Hashtable<String, String> m_EmoticsDict;
+
     public SearchPostsParser() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(App.getContext());
         m_SpoilerByButton = prefs.getBoolean("theme.SpoilerByButton", false);
-        m_EmoticsDict=Smiles.getSmilesDict();
+        m_EmoticsDict = Smiles.getSmilesDict();
     }
 
     public SearchResult searchResult;
 
     public String parse(String body) throws MessageInfoException, NotReportException {
 
-        Matcher m = Pattern.compile("<div class=\"borderwrap-header\">([\\s\\S]*?)<br /><div class=\"borderwrap\">([\\s\\S]*?)").matcher(body);
-        if (!m.find()) {
-            if (body.indexOf("К сожалению, Ваш поиск не дал никаких результатов") > 0)
-                throw new MessageInfoException("Поиск", "К сожалению, Ваш поиск не дал никаких результатов.\n" +
-                        "Попробуйте расширить параметры поиска, используя другое ключевое слово или изменением формата поиска.");
-            else
-                throw new NotReportException("Ошибка разбора результатов поиска");
-        }
-        searchResult = createSearchResult(m.group(1));
+
+        searchResult = createSearchResult(body);
 
 
         beginHtml("Результаты поиска");
 
         beginTopic(searchResult);
 
-        parsePosts(body);
+        Document doc = Jsoup.parse(body, "http://4pda.ru");
+        Elements postsElements = doc.select("div[data-post]");
+        for (Element element : postsElements) {
+            m_Body.append(parsePost(element));
+        }
 
-        endTopic( searchResult);
+        endTopic(searchResult);
         return m_Body.toString();
     }
 
@@ -113,73 +116,84 @@ public class SearchPostsParser extends HtmlBuilder  {
         endHtml();
     }
 
-
-    private StringBuilder parsePosts(String page) {
-        Matcher postMatcher = Pattern.compile("<div class=\"maintitle\">([\\s\\S]*?)(<div class=\"borderwrap\">|<div id=\"gfooter\">)").matcher("<div class=\"borderwrap\">" + page);
-
-        while (postMatcher.find()) {
-            m_Body.append(parsePost(postMatcher.group(1)));
-        }
-
-        return m_Body;
-    }
-
     private final Pattern postPattern = Pattern.compile("<div class=\"postcolor(?:\\s|\\w)*?\" id=\"post-\\d+\" style=\"height:300px;overflow-x:auto;\">([\\s\\S]*?)</div></td>\\s+</tr>\\s+<tr>\\s+<td class=\"row2\"></td>\\s+<td class=\"row2\">([\\s\\S]*?)</td>");
     private final Pattern topicPattern = Pattern.compile("<a href=\"/forum/index.php\\?showtopic=(\\d+)\">(.*?)</a></div>");
     private final Pattern userPattern = Pattern.compile("<span class=\"normalname\"><a href=\"/forum/index.php\\?showuser=(\\d+)\">(.*?)</a></span>[\\s\\S]*?<font color=\".*?\">\\[((offline)|(online))\\]");
     private final Pattern dateTimePattern = Pattern.compile("style=\"padding-bottom:2px\" />(.*?)</span>");
 
-    private String parsePost(String page) {
+    private String parsePost(Element element) {
         Boolean isWebviewAllowJavascriptInterface = Functions.isWebviewAllowJavascriptInterface(App.getInstance());
-        String s1 = null;
-        Matcher m = topicPattern.matcher(page);
-        if (m.find())
-            s1 = "<a href=\"http://4pda.ru/forum/index.php?showtopic=" + m.group(1) + "\">" + m.group(2) + "</a>";
 
-        m = userPattern.matcher(page);
-        String s2 = null;
-        if (m.find()) {
-            String userState = "online".equals(m.group(3)) ? "post_nick_online_cli" : "post_nick_cli";
-            s2 = "<span class=\"" + userState + "\"><a " + TopicBodyBuilder.getHtmlout(isWebviewAllowJavascriptInterface, "showUserMenu",
-                    m.group(1), m.group(2)) + " class=\"system_link\">" + m.group(2) + "</a></span>";
-        }
+        String topic = null;
+        String userId = null;
+        String userName = null;
+        String user = null;
+        String dateTime = null;
+        String userState = null;
+        String post = null;
+        String postFooter = null;
+        Element el = element.select("div.maintitle").first();
+        if (el != null)
+            topic = el.html();
 
-        String s3 = null;
-        m = dateTimePattern.matcher(page);
-        if (m.find()) {
-            s3 = m.group(1);
-        }
+        el = element.select("table.ipbtable>tbody").first();
+        if (el != null) {
+            Elements trElements = el.children();
 
-        String s4 = null;
-        String s5 = null;
-        m = postPattern.matcher(page);
-        if (m.find()) {
-            s4 = Post.modifyBody(m.group(1),m_EmoticsDict,true).replace("<br /><br />--------------------<br />", "");
+            Element trElement = trElements.get(0);
+            Elements tdElements = trElement.children();
+
+            Element tdElement = tdElements.get(0);
+            el = tdElement.select("a[href~=showuser=\\d+]").first();
+            if (el != null) {
+                Uri uri = Uri.parse(el.attr("href"));
+                userId = uri.getQueryParameter("showuser");
+                userName = el.text();
+            }
+            tdElement = tdElements.get(1);
+            dateTime = tdElement.text();
+
+            trElement = trElements.get(1);
+            tdElements = trElement.children();
+            tdElement = tdElements.get(0);
+            el = tdElement.select("span.postdetails font[color]:matches(\\[(online|offline)\\])").first();
+            if (el != null)
+                userState = "[online]".equals(el.text()) ? "post_nick_online_cli" : "post_nick_cli";
+
+            user = "<span class=\"" + userState + "\"><a " + TopicBodyBuilder.getHtmlout(isWebviewAllowJavascriptInterface, "showUserMenu",
+                    userId, userName) + " class=\"system_link\">" + userName + "</a></span>";
+
+            tdElement = tdElements.get(1);
+            post = Post.modifyBody(tdElement.html(), m_EmoticsDict, true).replace("<br /><br />--------------------<br />", "");
             if (m_SpoilerByButton) {
                 String find = "(<div class='hidetop' style='cursor:pointer;' )" +
                         "(onclick=\"var _n=this.parentNode.getElementsByTagName\\('div'\\)\\[1\\];if\\(_n.style.display=='none'\\)\\{_n.style.display='';\\}else\\{_n.style.display='none';\\}\">)" +
                         "(Спойлер \\(\\+/-\\).*?</div>)" +
                         "(\\s*<div class='hidemain' style=\"display:none\">)";
                 String replace = "$1>$3<input class='spoiler_button' type=\"button\" value=\"+\" onclick=\"toggleSpoilerVisibility\\(this\\)\"/>$4";
-                s4 = s4.replaceAll(find, replace);
+                post = post.replaceAll(find, replace);
             }
-            s5 = Post.modifyBody(m.group(2),m_EmoticsDict,true);
+            postFooter = Post.modifyBody(trElements.get(2).children().get(1).html(), m_EmoticsDict, true);
+
+            String POST_TEMPLATE = "<div class=\"between_messages\"></div>\n" +
+                    "<div class=\"topic_title_post\">%1s</div>\n" +
+                    "<div class=\"post_header\">\n" +
+                    "\t<table width=\"100%%\">\n" +
+                    "\t\t<tr><td>%2s</td>\n" +
+                    "\t<td><div align=\"right\"><span class=\"post_date_cli\">%3s</span></div></td>\n" +
+                    "</tr>\n" +
+                    "</table>" +
+                    "</div>" +
+                    "<div class=\"post_body emoticons\">%4s</div>" +
+                    "<div class=\"s_post_footer\"><table width=\"100%%\"><tr><td>%5s</td></tr></table></div>";
+            return String.format(POST_TEMPLATE, topic, user, dateTime, post, postFooter);
         }
 
 
+        return "";
+
         //Post.modifyBody(postMatcher.group(8));
-        String POST_TEMPLATE = "<div class=\"between_messages\"></div>\n" +
-                "<div class=\"topic_title_post\">%1s</div>\n" +
-                "<div class=\"post_header\">\n" +
-                "\t<table width=\"100%%\">\n" +
-                "\t\t<tr><td>%2s</td>\n" +
-                "\t<td><div align=\"right\"><span class=\"post_date_cli\">%3s</span></div></td>\n" +
-                "</tr>\n" +
-                "</table>" +
-                "</div>" +
-                "<div class=\"post_body emoticons\">%4s</div>" +
-                "<div class=\"s_post_footer\"><table width=\"100%%\"><tr><td>%5s</td></tr></table></div>";
-        return String.format(POST_TEMPLATE, s1, s2, s3, s4, s5);
+
     }
 
 
