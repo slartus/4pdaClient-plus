557e5f7beb551df1954e75384c4ca4bde46ae6a1
slartus <slartus@gmail.com>
2016-01-27 08:44:03 +0500

    так правильнее

--- forpdacommon/src/main/java/org/softeg/slartus/forpdacommon/Notification16_20.java
+++ forpdacommon/src/main/java/org/softeg/slartus/forpdacommon/Notification16_20.java
@@ -11,9 +11,9 @@ import android.os.Build;
  * Created by slinkin on 13.06.13.
  */
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-public class Notification16_x extends Notification11_15 {
+public class Notification16_20 extends Notification11_15 {
 
-    public Notification16_x(Context context, int icon, CharSequence tickerText, long when) {
+    public Notification16_20(Context context, int icon, CharSequence tickerText, long when) {
         super(context, icon, tickerText, when);
         mBuilder.setStyle(new Notification.InboxStyle());
     }
--- forpdacommon/src/main/java/org/softeg/slartus/forpdacommon/Notification21_x.java
+++ forpdacommon/src/main/java/org/softeg/slartus/forpdacommon/Notification21_x.java
@@ -6,20 +6,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 
-/**
+/*
  * Created by radiationx on 18.06.15.
  */
-public class Notification21_x extends Notification11_15{
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class Notification21_x extends Notification16_20{
     public Notification21_x(Context context, int icon, CharSequence tickerText, long when) {
         super(context, icon, tickerText, when);
-        mBuilder.setStyle(new Notification.InboxStyle());
+
         mBuilder.setColor(Color.argb(255,2,119,189));
     }
 
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public Notification createNotification() {
         return mBuilder.build();
