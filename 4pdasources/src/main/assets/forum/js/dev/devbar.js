bd8e8b3dd6e152dee47a13b1ebb3d1ab896ecc56
Alexey Sholohov <asolohov@gmail.com>
2016-03-17 10:38:46 +0300

    Обновление DEV Tools

--- 4pdasources/src/main/assets/forum/js/dev/devbar.js
+++ 4pdasources/src/main/assets/forum/js/dev/devbar.js
@@ -5,19 +5,50 @@
 	</div>\
 	<div id="dev-content"></div>\
 </div>' ) );
-	less.Parser().parse ( '\
+	/*less.Parser().parse ( */
+        less.render ( '\
 #devbar{\
-	position:fixed;width:2em;right:-1px;top:-1px;z-index:9999;background:#FFF!important;color:#000!important;padding:0!important;margin:0!important;border:solid 1px #000;\
+	position:fixed;\
+	right:0;\
+	top:0;\
+	z-index:9999;\
+	background:gray!important;\
+	color:#fff!important;\
+	padding:0!important;\
+	margin:0!important;\
 	font-size:16px!important;\
 	max-height:100%;\
-	textarea,input,button{\
-		box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;\
+	-moz-box-shadow: 0 3px 3px rgba(0,0,0,0.3);\
+	-webkit-box-shadow: 0 3px 3px rgba(0,0,0,0.3);\
+	input,button{\
+		box-sizing:border-box;\
+		-moz-box-sizing:border-box;\
+		-webkit-box-sizing:border-box;\
+		border-radius: 2px;\
+		font-weight: bold;\
+		color: #555;\
+		border: 1px solid grey;\
+		background:#fff;\
+	}\
+	 input:active, button:active{\
+		background:#ccc;\
+	}\
+	a {\
+		color: white !important;\
+	}\
+	textarea{\
+		box-sizing:border-box;\
+		-moz-box-sizing:border-box;\
+		-webkit-box-sizing:border-box;\
 	}\
 	.hide-element {\
 		display:none;\
 	}\
 	#dev-content {\
-		overflow:hidden;display:none;max-height:100%;overflow-y:auto;\
+		overflow:hidden;\
+		display:none;\
+		max-height:100%;\
+		overflow-y:auto;\
 		> *{\
 			display:none;\
 		}\
@@ -26,9 +57,10 @@
 		}\
 	}\
 	#dev-tabs {\
-		margin:0;float:left;\
+		margin:0;\
+		float:left;\
 		.btn {\
-			font-size:1em;\
+			font-size:16px;\
 			width:100%;\
 		}\
 		> * {\
@@ -39,25 +71,38 @@
 		}\
 	}\
 	&.open{\
-		width:auto;left:-1px;\
+		width:auto;\
+		left:-1px;\
 		display:block;\
 		#dev-content {\
 			display:block;\
 			h1 {\
-				font-size:1.3em;font-weight:bold;text-align:center;margin:0;padding:0;\
+				font-size:20px;\
+				font-weight:bold;\
+				text-align:center;\
+				margin:0;\
+				padding:0;\
 			}\
 		}\
 		#dev-tabs {\
-			margin: 0 .5em 0 0;\
+			margin: 0 8px 0 0;\
 			> * {\
 				display:block;\
 			}\
 		}\
 	}\
 	.btn {\
-		font-size:.8em;margin:0;padding:.5em;min-width:2em;line-height:1;\
+		font-size: 12px;\
+		margin:0;\
+		padding: 8px;\
+		min-width: 30px;\
+		line-height:1;\
 	}\
-}', function ( error, result ) { $('head').append ( '<style type="text/css">'+result.toCSS()+'</style>' ); } );
+}', {} ).then ( function(output){
+    $('head').append ( '<style type="text/css">'+output.css+'</style>' );
+}, function ( error ) {} ); /*, function ( error, result ) {
+    $('head').append ( '<style type="text/css">'+result.toCSS()+'</style>' ); }
+);*/
 
 	$('[data-toggle="open"][data-target="#devbar"]').click ( function () {
 		$(this).next().click();
--- 4pdasources/src/main/assets/forum/js/dev/devless.js
+++ 4pdasources/src/main/assets/forum/js/dev/devless.js
@@ -8,53 +8,43 @@
 	tabs.append ( '<button class="btn" data-tab="less">L</button><button class="btn" data-tab="edit">E</button>' );
 	content.append ( '<div data-tab-name="less">\
 	<h1>CSS/LESS</h1>\
-'+( window["DEVOUT"] && window["DEVOUT"]["showChooseCssDialog"] ? '<button class="btn" id="dev-less-file-compile">Выбрать стиль</button>' : '<input type="file" class="btn" id="dev-less-file-select"/> <button class="btn" id="dev-less-file-compile">Применить</button>' )+'\
-<br /><button id="dev-less-show" class="btn" data-toggle="hide-element" data-target="#dev-less-out"  disabled="disabled">Показать/скрыть итоговый код</button> \
-<textarea id="dev-less-out" style="width:100%;" class="hide-element" rows="10" readonly="readonly"></textarea> \
-<pre id="dev-less-error" style="color:red!important;"></pre><br /><br /> \
+'+( window["DEVOUT"] && window["DEVOUT"]["showChooseCssDialog"] ? '<input type="hidden" id="dev-less-file-path"><button class="btn" id="dev-less-file-compile">Выбрать стиль</button>' : '<input type="file" id="dev-less-file-select"/> <button class="btn" id="dev-less-file-compile">Применить</button>' )+'\
+<br /><br /><button id="dev-less-show" class="btn" data-toggle="hide-element" data-target="#dev-less-out"  disabled="disabled">Показать/скрыть итоговый код</button> \
+<textarea id="dev-less-out" style="width:100%; margin-right:10px;" class="hide-element" rows="10" readonly="readonly"></textarea> \
+<pre id="dev-less-error" style="display:block; background:#fff; color:red!important;"></pre>\
 </div>\
 <div data-tab-name="edit">\
 	<h1>EDIT</h1>\
 	<textarea id="dev-less-edit" style="width:100%;" rows="10"></textarea>\
-	<button id="dev-less-edit-compile" class="btn">Применить изменения</button><br /><br />\
+	<button id="dev-less-edit-compile" class="btn">Применить изменения</button><br />\
 </div>' );
 	$('head').append(styleElement);
 
 	function parseLess ( str ) {
-		var parser = less.Parser({
+                less.render (
+                    str,
+                    {
 			env: "development",
 			logLevel: 2,
 			async: false,
 			fileAsync: false,
 			poll: 1000,
 			functions: {},
-			dumpLineNumbers: "comments",
-		});
-		$('#dev-less-out').val('');
-		$('#dev-less-error').html('').hide();
-		$('#dev-less-edit').val ( str );
-		parser.parse ( str, function(error, result) {
-			if ( error == null ) {
-				try {
-					var out = result.toCSS();
-					styleElement.html(out);
-					$('#dev-less-out').val ( out );
-					$('#dev-less-show').removeAttr("disabled");
-				}
-				catch ( err ) {
-					$('#dev-less-out').val ( '' );
-					$('#dev-less-edit').val ( '' );
-					$('#dev-less-error').show().html ( 'Line '+err.line+' Index '+err.index+' Message: '+err.message+"\n"+err.extract.join("\n") );
-					$('#dev-less-show').attr ( "disabled", "disabled" );
-				}
-			}
-			else {
-				$('#dev-less-out').val ( '' );
-				$('#dev-less-edit').val ( '' );
-				$('#dev-less-error').show().html ( 'Line '+error.line+' Index '+error.index+' Message: '+error.message+"\n"+error.extract.join("\n") );
-				$('#dev-less-show').attr ( "disabled", "disabled" );
-			}
-		} );
+                        dumpLineNumbers: false,
+                    }
+                ).then (
+                    function ( output ) {
+                        styleElement.html ( output.css );
+                        $('#dev-less-out').val ( output.css );
+                        $('#dev-less-show').removeAttr("disabled");
+                    }
+                ,   function ( err ) {
+    			$('#dev-less-out').val ( '' );
+//    			$('#dev-less-edit').val ( '' );
+						$('#dev-less-error').show().html ( 'Line '+err.line+' Index '+err.index+' Message: '+err.message+"\n"+err.extract.join("\n") +' [<a href="#" onclick="$(&quot;#dev-less-error&quot;).html(&quot;&quot;).hide();$(&quot;#dev-less-show&quot;).removeAttr(&quot;disabled&quot;);return false;">hide</a>]' );
+    			$('#dev-less-show').attr ( "disabled", "disabled" );
+                    }
+                );
 	}
 	window["HtmlInParseLessContent"] = parseLess;
 
@@ -66,7 +56,12 @@
 	.on ( 'click', '#dev-less-file-compile', function ( ev ) {
 		ev.preventDefault();
 		if ( window["DEVOUT"] && window["DEVOUT"]["showChooseCssDialog"] ) {
-			DEVOUT.showChooseCssDialog();
+
+if ( $('#dev-less-file-path')[0].value.length==0 ) {
+				DEVOUT.showChooseCssDialog();
+			}else{
+				DEVOUT.acceptStyle($('#dev-less-file-path')[0].value);
+			}
 		}
 		else {
 			if ( $('#dev-less-file-select')[0].files.length != 1 ) {
@@ -81,7 +76,7 @@
 				}
 				else {
 					reader.onloadend = function ( evt ) {
-						if ( evt.target.readyState == FileReader.DONE ) { 
+						if ( evt.target.readyState == FileReader.DONE ) {
 							if ( evt.target.error ) {
 								var n = false;
 								for ( var m in evt.target.error ) {
