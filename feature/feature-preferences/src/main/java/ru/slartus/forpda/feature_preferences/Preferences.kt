175137606c2efce78c9dda29abbb731fc20ba984
slartus <slartus@gmail.com>
2021-09-27 07:05:25 +0500

    appPreference

--- feature/feature-preferences/src/main/java/ru/slartus/forpda/feature_preferences/Preferences.kt
+++ feature/feature-preferences/src/main/java/ru/slartus/forpda/feature_preferences/Preferences.kt
@@ -28,22 +28,10 @@ object Preferences {
         }
 
     @JvmStatic
-    var isHideFab: Boolean?
-        get() = getPreferences()!!
-            .getBoolean("fab.hide", true)
-        set(hide) {
-            getPreferences()!!
-                .edit().putBoolean("fab.hide", hide!!).apply()
-        }
+    var isHideFab: Boolean by appPreference("fab.hide", true)
 
     @JvmStatic
-    var isHideArrows: Boolean?
-        get() = getPreferences()!!
-            .getBoolean("hideArrows", false)
-        set(hide) {
-            getPreferences()!!
-                .edit().putBoolean("hideArrows", hide!!).apply()
-        }
+    var isHideArrows: Boolean by appPreference("hideArrows", false)
 
     @JvmStatic
     fun getFontSize(prefix: String): Int {
@@ -59,18 +47,10 @@ object Preferences {
 
     object Lists {
         @JvmStatic
-        val scrollByButtons: Boolean
-            get() = getPreferences()!!
-                .getBoolean("lists.scroll_by_buttons", false)
+        val scrollByButtons: Boolean by appPreference("lists.scroll_by_buttons", false)
 
         @JvmStatic
-        var lastSelectedList: String?
-            get() = getPreferences()!!
-                .getString("list.last_selected_list", "News_Pages")
-            set(listName) {
-                getPreferences()!!
-                    .edit().putString("list.last_selected_list", listName).apply()
-            }
+        var lastSelectedList: String by appPreference("list.last_selected_list", "News_Pages")
 
         @JvmStatic
         fun addLastAction(name: String) {
@@ -95,14 +75,10 @@ object Preferences {
                 .getString("lists.last_actions", "")!!.split("\\|").toTypedArray()
 
         @JvmStatic
-        val isRefresh: Boolean
-            get() = getPreferences()!!
-                .getBoolean("lists.refresh", true)
+        val isRefresh: Boolean by appPreference("lists.refresh", true)
 
         @JvmStatic
-        val isRefreshOnTab: Boolean
-            get() = getPreferences()!!
-                .getBoolean("lists.refresh_on_tab", true)
+        val isRefreshOnTab: Boolean by appPreference("lists.refresh_on_tab", true)
     }
 
     object List {
@@ -123,50 +99,34 @@ object Preferences {
                 .getString("$listName.list.sort", defaultValue)
         }
 
-        val startForumId: String?
-            get() = getPreferences()!!
-                .getString("Forum.start_forum_id", null)
+        val startForumId: String? by appPreference("Forum.start_forum_id", null)
 
         fun setStartForum(id: String?, title: String?) {
-            getPreferences()!!.edit()
-                .putString("Forum.start_forum_id", id)
-                .putString("Forum.start_forum_title", title)
-                .apply()
+            getPreferences()?.edit()
+                ?.putString("Forum.start_forum_id", id)
+                ?.putString("Forum.start_forum_title", title)
+                ?.apply()
         }
 
         object Favorites {
             @JvmStatic
-            val isLoadFullPagesList: Boolean
-                get() = getPreferences()!!
-                    .getBoolean("lists.favorites.load_all", false)
+            val isLoadFullPagesList: Boolean by appPreference("lists.favorites.load_all", false)
         }
     }
 
     object Forums {
-        val isShowImages: Boolean
-            get() = getPreferences()!!
-                .getBoolean("forum.list.show_images", true)
+        val isShowImages: Boolean by appPreference("forum.list.show_images", true)
     }
 
     object Topic {
         @JvmStatic
-        val readersAndWriters: Boolean
-            get() = getPreferences()!!
-                .getBoolean("theme.ShowReadersAndWriters", false)
+        val readersAndWriters: Boolean by appPreference("theme.ShowReadersAndWriters", false)
 
         @JvmStatic
-        val spoilFirstPost: Boolean
-            get() = getPreferences()!!
-                .getBoolean("theme.SpoilFirstPost", true)
+        val spoilFirstPost: Boolean by appPreference("theme.SpoilFirstPost", true)
 
         @JvmStatic
-        var confirmSend: Boolean
-            get() = getPreferences()!!
-                .getBoolean("theme.ConfirmSend", true)
-            set(value) {
-                getPreferences()!!
-                    .edit().putBoolean("theme.ConfirmSend", value).apply()
-            }
+        var confirmSend: Boolean by appPreference("theme.ConfirmSend", true)
 
         @JvmStatic
         val isShowAvatars: Boolean
@@ -178,39 +138,21 @@ object Preferences {
             }
 
         @JvmStatic
-        var showAvatarsOpt: Int
-            get() = ExtPreferences.parseInt(getPreferences(), "topic.show_avatar_opt", 0)
-            set(value) {
-                getPreferences()
-                    ?.edit()?.putInt("topic.show_avatar_opt", value)?.apply()
-            }
+        var showAvatarsOpt: Int by appPreference("topic.show_avatar_opt", 0)
 
         @JvmStatic
         val fontSize: Int
             get() = getFontSize("theme")
 
         @JvmStatic
-        val historyLimit: Int
-            get() = ExtPreferences.parseInt(getPreferences(), "topic.history_limit", 5)
+        val historyLimit: Int by appPreference("topic.history_limit", 5)
 
         object Post {
             @JvmStatic
-            var enableEmotics: Boolean?
-                get() = getPreferences()!!
-                    .getBoolean("topic.post.enableemotics", true)
-                set(value) {
-                    getPreferences()!!
-                        .edit().putBoolean("topic.post.enableemotics", value!!).apply()
-                }
+            var enableEmotics: Boolean by appPreference("topic.post.enableemotics", true)
 
             @JvmStatic
-            var enableSign: Boolean?
-                get() = getPreferences()!!
-                    .getBoolean("topic.post.enablesign", true)
-                set(value) {
-                    getPreferences()!!
-                        .edit().putBoolean("topic.post.enablesign", value!!).apply()
-                }
+            var enableSign: Boolean by appPreference("topic.post.enablesign", true)
 
             @JvmStatic
             fun addEmoticToFavorites(name: String) {
@@ -245,9 +187,7 @@ object Preferences {
 
     object System {
         @JvmStatic
-        val webviewCompatMode: Boolean
-            get() = getPreferences()!!
-                .getBoolean("webviewCompatMode", false)
+        val webviewCompatMode: Boolean by appPreference("webviewCompatMode", false)
 
         @JvmStatic
         var systemDir: String?
@@ -270,9 +210,7 @@ object Preferences {
          * Снимите галочку, если программа падает при вызове темы. Например, для Nook Simple Touch
          */
         @JvmStatic
-        val isShowWebViewScroll: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.WebViewScroll", true)
+        val isShowWebViewScroll: Boolean by appPreference("system.WebViewScroll", true)
 
         @JvmStatic
         fun isScrollUpButton(keyCode: Int): Boolean {
@@ -291,70 +229,43 @@ object Preferences {
         }
 
         @JvmStatic
-        val isDevSavePage: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.developerSavePage", false)
+        val isDevSavePage: Boolean by appPreference("system.developerSavePage", false)
 
         @JvmStatic
-        val isDevStyle: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.developerStyle", false)
+        val isDevStyle: Boolean by appPreference("system.developerStyle", false)
 
         @JvmStatic
-        val isDevGrid: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.developerGrid", false)
+        val isDevGrid: Boolean by appPreference("system.developerGrid", false)
 
         @JvmStatic
-        val isDevBounds: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.developerBounds", false)
+        val isDevBounds: Boolean by appPreference("system.developerBounds", false)
 
         @JvmStatic
-        val isCurator: Boolean
-            get() = getPreferences()!!
-                .getBoolean("system.curator", false)
+        val isCurator: Boolean by appPreference("system.curator", false)
 
         @JvmStatic
-        val drawerMenuPosition: String?
-            get() = getPreferences()!!
-                .getString("system.drawermenuposition", "left")
+        val drawerMenuPosition: String? by appPreference(
+            "system.drawermenuposition", "left"
+        )
 
         @JvmStatic
-        var isEvaluateJavascriptEnabled: Boolean
-            get() = getPreferences()
-                ?.getBoolean("system.EvaluateJavascriptEnabled", true) ?: true
-            set(value) {
-                getPreferences()!!
-                    .edit().putBoolean("system.EvaluateJavascriptEnabled", value).apply()
-            }
+        var isEvaluateJavascriptEnabled: Boolean by appPreference(
+            "system.EvaluateJavascriptEnabled", true
+        )
     }
 
     object News {
         @JvmStatic
-        var lastSelectedSection: Int
-            get() = getPreferences()!!
-                .getInt("news.lastselectedsection", 0)
-            set(section) {
-                getPreferences()!!
-                    .edit().putInt("news.lastselectedsection", section).apply()
-            }
+        var lastSelectedSection: Int by appPreference("news.lastselectedsection", 0)
 
         @JvmStatic
         val fontSize: Int
             get() = getFontSize("news")
 
         object List {
-            //            public static int getNewsListViewId() {
             @JvmStatic
             val isLoadImages: Boolean
                 get() = Preferences.isLoadImages
-            //
-            //                if ("medium".equals(App.getPreferences().getString("news.list.view", "full"))) {
-            //                    return R.layout.item_news_medium;
-            //                }
-            //                return R.layout.item_news;
-            //            }
         }
     }
 
@@ -373,13 +284,7 @@ object Preferences {
 
     object Files {
         @JvmStatic
-        var isConfirmDownload: Boolean
-            get() = getPreferences()!!
-                .getBoolean("files.ConfirmDownload", true)
-            set(b) {
-                getPreferences()!!
-                    .edit().putBoolean("files.ConfirmDownload", b).apply()
-            }
+        var isConfirmDownload by AppPreferenceBoolean("files.ConfirmDownload", true)
     }
 
     object Notifications {
@@ -413,9 +318,7 @@ object Preferences {
             }
 
             @JvmStatic
-            val isReadDone: Boolean
-                get() = getPreferences()!!
-                    .getBoolean("refreshQMSData", false)
+            val isReadDone: Boolean by appPreference("refreshQMSData", false)
         }
     }
 
@@ -429,36 +332,6 @@ object Preferences {
                 .edit().putString("notes.placement", value).apply()
         }
 
-        var remoteUrl: String? by PreferenceStringDelegate("notes.remote.url","")
-//            get() = getPreferences()!!
-//                .getString("notes.remote.url", "")
-//            set(value) {
-//                getPreferences()!!
-//                    .edit().putString("notes.remote.url", value).apply()
-//            }
-    }
-
-    data class PreferenceStringDelegate(
-        private val key: String,
-        private val defaultValue: String
-    ) : ReadWriteProperty<String?, String?> {
-
-        private var trimmedValue: String = ""
-
-        operator fun getValue(
-            thisRef: String?,
-            property: KProperty<*>
-        ): String {
-            return getPreferences()
-                ?.getString(key, defaultValue) ?: defaultValue
-        }
-
-        operator fun setValue(
-            thisRef: String?,
-            property: KProperty<*>, value: String?
-        ) {
-            getPreferences()?.edit()?.putString(key, value)?.apply()
-        }
-
+        var remoteUrl by appPreference("notes.remote.url", "")
     }
 }
--- feature/feature-preferences/src/main/java/ru/slartus/forpda/feature_preferences/PreferencesUtils.kt
+++ feature/feature-preferences/src/main/java/ru/slartus/forpda/feature_preferences/PreferencesUtils.kt
@@ -4,15 +4,74 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.util.TypedValue
 import androidx.preference.PreferenceManager
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 val Context.preferences: SharedPreferences
     get() = PreferenceManager.getDefaultSharedPreferences(this)
 
-@Suppress("SameParameterValue")
 fun Context?.getAttr(attr: Int, fallbackAttr: Int): Int {
     val value = TypedValue()
     this?.theme?.resolveAttribute(attr, value, true)
     return if (value.resourceId != 0) {
         attr
     } else fallbackAttr
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> appPreference(key: String, defaultValue: T): ReadWriteProperty<Any?, T> {
+    return when (T::class) {
+        String::class -> {
+            AppPreferenceString(key, defaultValue as String?) as ReadWriteProperty<Any?, T>
+        }
+        Boolean::class -> {
+            AppPreferenceBoolean(key, defaultValue as Boolean) as ReadWriteProperty<Any?, T>
+        }
+        Int::class -> {
+            AppPreferenceInt(key, defaultValue as Int) as ReadWriteProperty<Any?, T>
+        }
+        else -> throw Exception("appPreference given unknown class ${T::class}")
+    }
+}
+
+data class AppPreferenceString(
+    val key: String,
+    val defaultValue: String?
+) : ReadWriteProperty<Any?, String?> {
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String?) {
+        App.getPreferences()?.edit()?.putString(key, value)?.apply()
+    }
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): String? {
+        return App.getPreferences()
+            ?.getString(key, defaultValue) ?: defaultValue
+    }
+}
+
+data class AppPreferenceBoolean(
+    private val key: String,
+    private val defaultValue: Boolean
+) : ReadWriteProperty<Any?, Boolean> {
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): Boolean {
+        return App.getPreferences()?.getBoolean(key, defaultValue) ?: defaultValue
+    }
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
+        App.getPreferences()?.edit()?.putBoolean(key, value)?.apply()
+    }
+}
+
+data class AppPreferenceInt(
+    private val key: String,
+    private val defaultValue: Int
+) : ReadWriteProperty<Any?, Int> {
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        val strValue =
+            App.getPreferences()?.getString(key, defaultValue.toString()) ?: defaultValue.toString()
+        return strValue.toIntOrNull() ?: defaultValue
+    }
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        App.getPreferences()?.edit()?.putInt(key, value)?.apply()
+    }
 }
