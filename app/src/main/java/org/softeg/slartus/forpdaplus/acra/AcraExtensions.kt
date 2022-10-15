a2cf863eb47cc81551469bca20723189d8801401
Artem Slinkin <slartus@gmail.com>
2022-10-15 09:43:26 +0500

    JobManager.create

--- app/src/main/java/org/softeg/slartus/forpdaplus/acra/AcraExtensions.kt
+++ app/src/main/java/org/softeg/slartus/forpdaplus/acra/AcraExtensions.kt
@@ -1,6 +1,7 @@
 package org.softeg.slartus.forpdaplus.acra
 
 import android.app.Application
+import com.evernote.android.job.JobManager
 import org.acra.config.limiter
 import org.acra.config.toast
 import org.acra.ktx.initAcra
@@ -8,6 +9,7 @@ import org.softeg.slartus.forpdaplus.BuildConfig
 import org.softeg.slartus.forpdaplus.R
 
 fun Application.configureAcra(){
+    JobManager.create(this)
     initAcra {
         buildConfigClass = BuildConfig::class.java
         toast {
